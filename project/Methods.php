d025a63969d6c3bb58460a2a4650466061587898
frunolu <fruno.lu@gmail.com>
2024-01-12 22:34:21 +0100

    add efficientMethod()

--- project/Methods.php
+++ project/Methods.php
@@ -44,4 +44,31 @@ class Methods
 
     }
 
+
+    // identifikoval som laravel eloquent funkcie
+
+    public static function efficientMethod($user_id): array
+    {
+        $all_certificates = [];
+
+        $courses = Course::where('active', 1)->get();
+
+        $author_courses = $courses->where('author', $user_id);
+
+        $phpCertificates = $courses->filter(function ($course) {
+            return strpos($course->certificate, 'php') !== false;
+        })->pluck('certificate')->toArray();
+
+        $all_certificates = $courses->pluck('certificate')->toArray();
+
+        $unique_certificates = array_unique($all_certificates);
+
+        return [
+            'author_courses' => $author_courses,
+            'php_certificates' => $phpCertificates,
+            'all_certificates' => $all_certificates,
+            'unique_certificates' => $unique_certificates,
+        ];
+    }
+
 }
